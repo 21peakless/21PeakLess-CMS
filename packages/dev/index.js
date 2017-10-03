5c466a820b2bab62670eb0d058f479233b87754e
Jan Biasi <jan.biasi@namics.com>
2017-10-03 16:25:57 +0200

    chore(app): Starting with dev env

--- packages/dev/index.js
+++ packages/dev/index.js
@@ -1,5 +1,20 @@
+const { join } = require('path');
 const config = require('@21peakless/config');
 const server = require('@21peakless/server');
 const http = require('@21peakless/http');
 
 console.log(config, server, http);
+
+const app = server({
+    middlewares: [],
+    statics: {
+        '/admin': join(__dirname, '..', 'client/public'),
+    },
+});
+
+const cdn = server({
+    middlewares: [],
+    statics: {},
+});
+
+app.use('/api/v1/', cdn);
